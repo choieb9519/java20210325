251b07f7434d196b219b3bfc39145ceba1c9989d
choieb9519 <choieb9519@gmail.com>
2021-03-25 11:32:58 +0900

    chapter 01

---
+++ src/p01/textbook/Hello.java
@@ -0,0 +1,14 @@
+package p01.textbook;
+
+/*
+ * 작성자: 신용권
+ * 작성일: 2015.12.31
+ */
+public class Hello {
+		//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 실행하는 실행문
+		System.out.println("Hello, welcome to java world!");
+	
+	}//end of mail
+}//end of class
