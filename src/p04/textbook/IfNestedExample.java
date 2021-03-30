d3e98e3a098e97e5e2589bd4e26056a96be20c11
choieb9519 <choieb9519@gmail.com>
2021-03-30 16:05:46 +0900

    20210330

--- src/p04/textbook/IfNestedExample.java
+++ src/p04/textbook/IfNestedExample.java
@@ -3,6 +3,7 @@ package p04.textbook;
 public class IfNestedExample {
 	public static void main(String[] args) {
 		int score = (int)(Math.random() * 20) + 81;
+					//81 이상 100이하의 수를 추출하기 위한 코드
 		System.out.println("점수: " + score);
 		
 		String grade;
