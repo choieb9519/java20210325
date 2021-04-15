78469510b2920ed4e8915ca1b715297c948bfaec
choieb9519 <choieb9519@gmail.com>
2021-04-15 11:42:30 +0900

    20210415

--- src/p08/textbook/ex4/OracleDao.java
+++ src/p08/textbook/ex4/OracleDao.java
@@ -3,21 +3,21 @@ package p08.textbook.ex4;
 public class OracleDao implements DataAccessObject{
 	@Override
 	public void select() {
-		System.out.println("MySql DB에서 검색");
+		System.out.println("Oracle DB에서 검색");
 	}
 
 	@Override
 	public void insert() {
-		System.out.println("MySql DB에 삽입");
+		System.out.println("Oracle DB에 삽입");
 	}
 
 	@Override
 	public void update() {
-		System.out.println("MySql DB를 수정");
+		System.out.println("Oracle DB를 수정");
 	}
 
 	@Override
 	public void delete() {
-		System.out.println("MySql DB에서 삭제");
+		System.out.println("Oracle DB에서 삭제");
 	}
 }
