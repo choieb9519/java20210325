cf7180f9ca490e893a2915c558d55386826ac495
choieb9519 <choieb9519@gmail.com>
2021-05-03 16:42:33 +0900

    20210503

---
+++ src/p18/lecture/A01IO.java
@@ -0,0 +1,17 @@
+package p18.lecture;
+
+public class A01IO {
+	/*InputStream: 입력 기반 최상위 클래스
+					추상클래스라서 서브클래스인 FileInputStream을 가장 많이 사용
+					read 메소드, 1byte씩 읽음
+					오버로드는 입력한 byte씩 읽음
+	OutputStream: 출력 기반 최상위 클래스
+	: byte 단위 입출력 클래스
+	
+	Reader: 입력 기반 최상위 클래스
+	Writer: 출력 기반 최상위 클래스
+	: 문자 단위 입출력 클래스
+	
+	입출력의 기준은 실행되는 프로그램이 기준*/
+	
+}
